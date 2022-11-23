2dd0efffa6cfe67c18fa6bffa21ba30cb52b81c6
aaltgod <minealaska29@gmail.com>
2022-11-24 02:53:35 +0300

    add tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use std::error::Error;
+use std::io::Write;
+use tokio::net::TcpListener;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:1337").await?;
+    let (socket, _) = listener.accept().await?;
     
+    println!("listener were accepeted");
+
+    let mut stream = socket.into_std()?;
+    stream.set_nonblocking(false)?;
+    stream.write_all(b"FLAG")?;
+    
+    Ok(())
 }
