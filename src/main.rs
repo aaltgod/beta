a9f084fff0220edc32c4a97922b9eb4e7ab48468
aaltgod <minealaska29@gmail.com>
2022-11-27 03:34:20 +0300

    add hyper

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
-use std::error::Error;
-use std::io::Write;
-use tokio::net::TcpListener;
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::str::FromStr;
 
+use hyper::{Server, Client, Request, Response, Body, Uri};
+use hyper::service::{make_service_fn, service_fn};
+
+
+async fn handler(mut req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    println!("{:?} {}", req.headers(), req.uri().path());
+
+    let client = Client::new();
+    
+    let forwarded_uri = String::from("http://127.0.0.1:4554/");
+
+    match Uri::from_str(forwarded_uri.as_str()) {
+        Ok(uri) => *req.uri_mut() = uri,
+        // TODO: never PANIC
+        Err(e) => panic!("{}", e)
+    };
+
+    let result: Response<Body>;
+
+    match client.request(req).await {
+        Ok(resp) => result = resp,
+        // TODO: never PANIC
+        Err(e) => panic!("{}", e) 
+    };
+    
+    Ok(Response::from(result))
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("127.0.0.1:1337").await?;
-    let (socket, _) = listener.accept().await?;
-    
-    println!("listener were accepeted");
+async fn main() {
+    let addr = SocketAddr::from(([0, 0, 0, 0], 1337));
 
-    let mut stream = socket.into_std()?;
-    stream.set_nonblocking(false)?;
-    stream.write_all(b"FLAG")?;
-    
-    Ok(())
+    let make_service = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(handler))
+    });
+
+    let server = Server::bind(&addr).serve(make_service);
+
+    if let Err(e) = server.await {
+        eprintln!("Fatal err {}", e)
+    }
 }
