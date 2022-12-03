aa0f530fbf55f36483c307405a2779a14ea777f3
aaltgod <minealaska29@gmail.com>
2022-12-03 04:27:24 +0300

    add change_uri

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,25 @@ pub mod config;
 use config::Config;
 
 use bytes::Bytes;
+use hyper::http::HeaderValue;
 use regex::Regex;
 
 use hyper::{Server, Client, Request, Response, Body, Uri};
 use hyper::service::{make_service_fn, service_fn};
 
+async fn change_uri(uri: Uri, host: &HeaderValue) -> Uri {
+    let re = Regex::new("([A-Z0-9]{31})=").unwrap();
 
+    let changed_uri = Uri::builder().
+        scheme("http").
+        authority(host.to_str().unwrap()).
+        path_and_query(re.replace_all(uri.path_and_query().unwrap().as_str(), "FLAG").to_string()).
+        build().
+        expect("build new uri");
+
+    println!("{:?}", changed_uri);
+    changed_uri
+}
 async fn change_body(body_bytes: Bytes) -> Result<Body, hyper::Error> {
     let re = Regex::new("([A-Z0-9]{31})%3D").unwrap();
     if !body_bytes.is_empty(){
@@ -26,12 +39,7 @@ async fn change_body(body_bytes: Bytes) -> Result<Body, hyper::Error> {
 async fn change_request(req: Request<Body>) -> Result<Request<Body>, hyper::http::Error> {
    let (parts, body) = req.into_parts();
    let new_host = parts.headers.get("host").unwrap();
-   let new_uri = Uri::builder().
-        scheme("http").
-        authority(new_host.to_str().unwrap()).
-        path_and_query(parts.uri.path_and_query().unwrap().to_string()).
-        build().
-        expect("build new uri");
+   let new_uri = change_uri(parts.uri, new_host).await;
    
    let body_bytes = hyper::body::to_bytes(body).await.expect("body to bytes");
    println!("body: {:?}\n{}", body_bytes, new_uri);
@@ -43,16 +51,16 @@ async fn change_request(req: Request<Body>) -> Result<Request<Body>, hyper::http
     changed_request
 }
 
-async fn upstream_request(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+async fn do_request(config: Config, req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     println!("{:?}", req.uri());
 
-    let filtered_req = change_request(req).await.unwrap();
-    let upstream_resp = Client::new().request(filtered_req).await.map_err(|e| {
+    let changed_req = change_request(req).await.unwrap();
+    let service_resp = Client::new().request(changed_req).await.map_err(|e| {
         eprintln!("{:?}", e);
         hyper::Error::from(e)
     });
 
-    let mut resp = upstream_resp.unwrap();
+    let mut resp = service_resp.unwrap();
     let _res = resp.headers_mut().insert("Privet", "ww".parse().unwrap()).is_none();
     
     Ok(resp)  
@@ -73,8 +81,9 @@ async fn run_server(config: Config) {
     }
 
     let make_service = make_service_fn(move |_| { 
+        let c = config.clone();
         async move {
-             Ok::<_, hyper::Error>(service_fn(move |req| upstream_request(req)))
+             Ok::<_, hyper::Error>(service_fn(move |req| do_request(c.clone(), req)))
         }
     });
 
