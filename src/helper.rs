1ee206df27cae31aaa41de9c1b69e5b68f6a0b74
aaltgod <minealaska29@gmail.com>
2025-01-08 13:52:56 +0300

    fix

--- src/helper.rs
+++ src/helper.rs
@@ -15,9 +15,27 @@ impl FlagsProvider for Helper {
         let mut rng = rand::thread_rng();
 
         let chars = alphabet.chars().collect::<Vec<char>>();
-        (0..length)
-            .map(|_| chars[rng.gen_range(0..chars.len() - postfix.len())])
+        (0..length - postfix.len())
+            .map(|_| chars[rng.gen_range(0..chars.len())])
             .collect::<String>()
             + postfix
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_build_flag() {
+        let helper = Helper::new();
+        let alphabet = "abcdef";
+        let length = 5;
+        let postfix = "=";
+
+        let flag = helper.build_flag(alphabet, length, postfix);
+
+        assert_eq!(flag.len(), length);
+        assert_eq!(flag.ends_with(postfix), true);
     }
 }
