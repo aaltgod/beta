07f287901196f590f80a9674764997c570d7b1c0
aaltgod <minealaska29@gmail.com>
2025-01-23 23:53:39 +0300

    #43 add gzip encoding/decoding of response body

--- e2e/checker/test_checker.py
+++ e2e/checker/test_checker.py
@@ -2,6 +2,7 @@ import requests
 import random
 import string
 import urllib.parse
+import pytest
 
 
 url = 'http://test-beta:5000/echo'
@@ -25,14 +26,24 @@ def send_form(url, flag: str):
     received_flag = parsed_response.get('data', [None])[0]
     return received_flag
 
-def test_flag_in_json():
-    expected_flag = generate_flag()
-    received_flag = send_json(url, expected_flag)
+# flags_quantity=1 - default case
+# flags_quantity=2 - test for processing of multiple flags
+# flags_quantity=20 - triggers server-side encoding by sending a large payload, this is for encoding/decoding testing of `beta`
+@pytest.mark.parametrize("flags_quantity,repeats", [(1, 1), (2, 1), (20, 1)])
+def test_flag_in_json(flags_quantity, repeats):
+    for _ in range(repeats):
+        expected_flag = ''.join([generate_flag() for _ in range(flags_quantity)])
+        received_flag = send_json(url, expected_flag)
 
-    assert received_flag == expected_flag, f"Expected flag is {expected_flag}, received flag is {received_flag}"
+        assert received_flag == expected_flag, f"Expected flag is {expected_flag}, received flag is {received_flag}"
 
-def test_flag_in_form():
-    expected_flag = generate_flag()
-    received_flag = send_form(url, expected_flag)
+# flags_quantity=1 - default case
+# flags_quantity=2 - test for processing of multiple flags
+# flags_quantity=20 - triggers server-side encoding by sending a large payload, this is for encoding/decoding testing of `beta`
+@pytest.mark.parametrize("flags_quantity,repeats", [(1, 1), (2, 1), (20, 1)])
+def test_flag_in_form(flags_quantity, repeats):
+    for _ in range(repeats):
+        expected_flag = ''.join([generate_flag() for _ in range(flags_quantity)])
+        received_flag = send_form(url, expected_flag)
 
-    assert received_flag == expected_flag, f"Expected flag is {expected_flag}, received flag is {received_flag}"
+        assert received_flag == expected_flag, f"Expected flag is {expected_flag}, received flag is {received_flag}"
