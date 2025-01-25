57560b715e5ceb7bbe550f134da7d3133695c4e9
aaltgod <minealaska29@gmail.com>
2025-01-25 18:02:59 +0300

    #10 remove `unwrap`s (#13)

--- src/handlers.rs
+++ src/handlers.rs
@@ -8,7 +8,7 @@ pub async fn metrics_handler() -> Result<impl reply::Reply, warp::Rejection> {
     let encoder = prometheus::TextEncoder::new();
     let mut buffer = Vec::new();
     if let Err(e) = encoder.encode(&REGISTRY.gather(), &mut buffer) {
-        error!("couldn't encode requests metrics: {}", e);
+        error!("couldn't encode requests metrics: {e}");
 
         return Ok(reply::with_status(
             "INTERNAL_SERVER_ERROR".to_string(),
@@ -19,7 +19,7 @@ pub async fn metrics_handler() -> Result<impl reply::Reply, warp::Rejection> {
     let mut result = match String::from_utf8(buffer.clone()) {
         Ok(res) => res,
         Err(e) => {
-            error!("couldn't convert metrics from utf8 to String: {}", e);
+            error!("couldn't convert metrics from utf8 to String: {e}");
 
             return Ok(reply::with_status(
                 "INTERNAL_SERVER_ERROR".to_string(),
@@ -31,7 +31,7 @@ pub async fn metrics_handler() -> Result<impl reply::Reply, warp::Rejection> {
     buffer.clear();
 
     if let Err(e) = encoder.encode(&prometheus::gather(), &mut buffer) {
-        error!("couldn't encode prometheus metrics: {}", e);
+        error!("couldn't encode prometheus metrics: {e}");
 
         return Ok(reply::with_status(
             "INTERNAL_SERVER_ERROR".to_string(),
@@ -42,7 +42,7 @@ pub async fn metrics_handler() -> Result<impl reply::Reply, warp::Rejection> {
     let prometheus_result = match String::from_utf8(buffer) {
         Ok(v) => v,
         Err(e) => {
-            error!("couldn't convert metric from utf8 to String: {}", e);
+            error!("couldn't convert metric from utf8 to String: {e}");
 
             return Ok(reply::with_status(
                 "INTERNAL_SERVER_ERROR".to_string(),
